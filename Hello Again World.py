30a4d8f01cc9c8683b86a45ab269e6560ec4ddf3
bluntforcefive <chazmertes@gmail.com>
2022-03-15 18:07:38 -0400

    Hello World Again (this time on Main Street)

---
+++ Hello Again World.py
@@ -0,0 +1,6 @@
+'''
+Hello World but again so that I can push to the main branch instead of the master
+Not that I understand why you would want to push to one or the other, but I'm sure I'll get to that point.
+'''
+
+print("Hello again...world")
